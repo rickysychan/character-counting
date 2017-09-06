fe38d953c8635064d3a99de68190be3d8f55fd9b
Ricky Chan <rickysychan@gmail.com>
2017-09-06 16:47:45 +0000

    completed

--- character-counting.js
+++ character-counting.js
@@ -1,15 +1,38 @@
 // // Write a function countLetters that can return to us all
 // the unique characters that exist in a string that is passed into
 // the function.
-const letters = [];
+
 
 function countLetters(sentence){
-const splitWords = sentence.split(' ').join('');
-const splitLetters = splitWords.split('');
+  const splitWords = sentence.split(' ').join('');
+  const splitLetters = splitWords.split('');
+  let letters = {};
 
-return splitLetters;
+  for(let i = 0; i < splitLetters.length; i++){
+    const char = splitLetters[i];
+
+    /* loops through the length of SplitLetters and
+       stores them onto char
+    */
+    if (letters[char] === undefined){
+      letters[char] = 1;
+      /*
+      this adds char the object letters as keys
+      checks to see the characters are undefined because it has
+      not occured before. it is undefined because it was not added as a key.
+       if it is undefined the value is set to 1
+
+      */
+    } else {
+      letters[char] += 1;
+/* if it is defined add one to offset the counter which starts at undefined
+*/
+    }
+  }
+
+
+  return letters;
 }
-
 
 console.log(countLetters("lighthouse in the house"));
 
